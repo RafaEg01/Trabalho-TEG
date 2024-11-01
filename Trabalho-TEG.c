4aa1ef017dce0b341c4b6037ba7c23eb291d903c
Groudon19 <gustavopiacentinisilva@gmail.com>
2024-11-01 00:08:39 -0300

    Major commitgit status! Calculo de médias alterado e case 3 de gerar um relatório incrementado para incluir a acurácia no arquivo

--- Trabalho-TEG.c
+++ Trabalho-TEG.c
@@ -15,6 +15,7 @@ typedef struct flower{
 
  typedef struct Dados{ // Parametros para realizar o relatorio
         float *limiares;
+        float *accuracies;
         int *tamanhoGrupos;
         int tamanho;
     }dados;
@@ -88,7 +89,7 @@ int main() {
 
         for(int i = 0; i < 149; i++){
             for(int j = i+1; j < 150; j++){
-                if(matrix[i][j] <= 0.08){
+                if(matrix[i][j] <= 0.05){
                     matriz_adjacencia[i][j] = 1; //tem relacao
                     matriz_adjacencia[j][i] = 1;
                 }
@@ -149,8 +150,9 @@ int main() {
         float limiteSuperior,limiteInferior,incrementar;
 
         dados dados;            // Tirar daqui depois e adiconar uma opção nova de gerar o relatorio
-        dados.tamanhoGrupos = NULL;
         dados.limiares = NULL;
+        dados.accuracies = NULL;
+        dados.tamanhoGrupos = NULL;
         dados.tamanho = 0;
 
         FILE *relatorio = fopen("IrisDataset.csv", "r");
@@ -164,9 +166,13 @@ int main() {
 
         // Lê o resto das linhas
         for(int i = 0; i < 150; i++){
-            total++;
-            fscanf(relatorio, "%*[^,],%f,%f,%f,%f\n", &flores[i].sepLength, &flores[i].sepWidth, &flores[i].petLength, &flores[i].petWidth); //tem que usar [^,] pq a string da variedade não tem o \0 
-            //printf("Flower %i: %f\t%f\t%f\t%f\n", i+1, flowers[i].petLength, flowers[i].petWidth, flowers[i].sepLength, flowers[i].sepWidth);
+            fscanf(relatorio, "%[^,],%f,%f,%f,%f\n", tipo_flor, &flores[i].sepLength, &flores[i].sepWidth, &flores[i].petLength, &flores[i].petWidth);
+            if(!strcmp(tipo_flor, "Setosa")){
+                flores[i].tipo = 1;
+            } else {
+                flores[i].tipo = 0;
+            }
+            // printf("Flower %i: %f\t%f\t%f\t%f\t%i\n", i+1, flores[i].sepLength, flores[i].sepWidth, flores[i].petLength, flores[i].petWidth, flores[i].tipo);
         }
 
         printf("\nQual valor para o limite Superior\n");
@@ -242,10 +248,12 @@ int main() {
                 tamanhos_dos_grupos[0]++;
                 grupos[0] = realloc(grupos[0], sizeof(int)*tamanhos_dos_grupos[0]);
                 grupos[0][tamanhos_dos_grupos[0] - 1] = grupos[i][j];
+                media0 = centro(grupos[0], tamanhos_dos_grupos[0], flores); // Recalcula o centro a cada ponto novo
             } else {
                 tamanhos_dos_grupos[1]++;
                 grupos[1] = realloc(grupos[1], sizeof(int)*tamanhos_dos_grupos[1]);
                 grupos[1][tamanhos_dos_grupos[1] - 1] = grupos[i][j];
+                media1 = centro(grupos[1], tamanhos_dos_grupos[1], flores);
             }
         }
         num_grupos--;
@@ -472,6 +480,7 @@ void infoRelatorio(flower flores[], dados* dados, float limiteSuperior, float li
         bool visitado[150] = {false};
         int matriz_adjacencia[150][150] = {0};
         float matrix[150][150] = {0};
+        int TP = 0, FP = 0, TN = 0, FN = 0; 
      
         
         
@@ -482,20 +491,36 @@ void infoRelatorio(flower flores[], dados* dados, float limiteSuperior, float li
         distancia_normalizada(&demaior, &demenor, matrix);
         
         for(int i = 0; i < 149; i++){
-                    for(int j = i+1; j < 150; j++){
-                        if(matrix[i][j] <= limiar){
-                            matriz_adjacencia[i][j] = 1; //tem relacao
-                            matriz_adjacencia[j][i] = 1;
-                        }
-                    }
+            for(int j = i+1; j < 150; j++){
+                if(matrix[i][j] <= limiar){
+                    matriz_adjacencia[i][j] = 1; //tem relacao
+                    matriz_adjacencia[j][i] = 1;
                 }
-     
+            }
+        }
+
+        // Salva o limiar usado nos dados
+        float *limiartemp = (float*)realloc(dados->limiares, dados->tamanho * sizeof(float)); // Alocar mais espaço a cada iteração +1
+        if (limiartemp == NULL) { // Testar caso a alocação tenha problemas
+            printf("Falha na alocação de memória\n");
+            free(dados->limiares);
+            exit(1);
+        }
+        dados->limiares = limiartemp;
+        dados->limiares[dados->tamanho-1] = limiar; // Colocar valor na ultima posição do Vetor
 
         
         int* tamanhos_dos_grupos = malloc(sizeof(int));
         tamanhos_dos_grupos[0] = 0;
         int** grupos = malloc(sizeof(int*));
         *(grupos) = malloc(sizeof(int));
+        if(grupos == NULL || *grupos == NULL || tamanhos_dos_grupos == NULL){
+            printf("Falha na alocação de memória\n");
+            free(*grupos);
+            free(grupos);
+            free(tamanhos_dos_grupos);
+            exit(1);
+        }
 
         int num_grupos = 0;
 
@@ -516,19 +541,6 @@ void infoRelatorio(flower flores[], dados* dados, float limiteSuperior, float li
 
         } // O while deve parar quando todo o vetor de visitados for true
         while(visitado[ponto] != true);
-        // printf("\nTamanho do dados = %i", dados->tamanho);a
-        // printf("\nTamanho do limiar = %f", limiar);
-        // printf("\nNumero de grupos: %i\n", num_grupos + 1);
-
-        float *limiartemp = (float*)realloc(dados->limiares, dados->tamanho * sizeof(float)); // Alocar mais espaço a cada iteração +1
-        if (limiartemp == NULL) { // Testar caso a alocação tenha problemas
-            printf("Falha na alocação de memória\n");
-            free(dados->limiares);
-            exit(1);
-        }
-        dados->limiares = limiartemp;
-        dados->limiares[dados->tamanho-1] = limiar; // Colocar valor na ultima posição do Vetor
-
 
         int *tamanhoGrupostemp = (int*)realloc(dados->tamanhoGrupos, dados->tamanho * sizeof(int)); // Alocar mais espaço a cada iteração +1
         if (tamanhoGrupostemp == NULL) {      // Testar caso a alocação tenha problemas
@@ -539,12 +551,97 @@ void infoRelatorio(flower flores[], dados* dados, float limiteSuperior, float li
         dados->tamanhoGrupos = tamanhoGrupostemp;
         dados->tamanhoGrupos[dados->tamanho-1] = num_grupos+1; // Colocar valor na ultima posição do Vetor
 
+        for(int i = 0; i < num_grupos; i++){
+            int ind_maior = i;
+
+            for(int j = i + 1; j < num_grupos + 1; j++){
+                if(tamanhos_dos_grupos[j] > tamanhos_dos_grupos[ind_maior]){
+                    ind_maior = j;
+                }
+            }
+
+            int temp = tamanhos_dos_grupos[i];
+            int* ptemp = grupos[i]; // ponteiro temporario
+            tamanhos_dos_grupos[i] = tamanhos_dos_grupos[ind_maior];
+            grupos[i] = grupos[ind_maior];
+            tamanhos_dos_grupos[ind_maior] = temp;
+            grupos[ind_maior] = ptemp;
+        }
+
+        // Convencionamos que o grupos[0] (maior) é o grupo de não setosas e o grupos[1] (segundo maior) é o grupo de setosas (Melhor trocar isso dps)
+        flower media0 = centro(grupos[0], tamanhos_dos_grupos[0], flores);
+        flower media1 = centro(grupos[1], tamanhos_dos_grupos[1], flores);
+
+        // Junta os outros grupos aos dois primeiros
+        for(int i = num_grupos; i > 1; i--){
+            for(int j = 0; j < tamanhos_dos_grupos[i]; j++){
+                // Compara a DE entre os vértices de cada grupo pequeno separado com os centros dos dois grupos principais
+                if(distancia_euclideana(flores[grupos[i][j]], media0) <= distancia_euclideana(flores[grupos[i][j]], media1)){
+                    tamanhos_dos_grupos[0]++;
+                    grupos[0] = realloc(grupos[0], sizeof(int)*tamanhos_dos_grupos[0]);
+                    grupos[0][tamanhos_dos_grupos[0] - 1] = grupos[i][j];
+                    media0 = centro(grupos[0], tamanhos_dos_grupos[0], flores); // Recalcula o centro a cada ponto novo
+                } else {
+                    tamanhos_dos_grupos[1]++;
+                    grupos[1] = realloc(grupos[1], sizeof(int)*tamanhos_dos_grupos[1]);
+                    grupos[1][tamanhos_dos_grupos[1] - 1] = grupos[i][j];
+                    media1 = centro(grupos[1], tamanhos_dos_grupos[1], flores);
+                }
+            }
+            num_grupos--;
+            free(grupos[i]);
+        }
+
+        // Classifica o grupo com mais Setosas como o de Setosas e o outro como o de não Setosas
+        int *Setosas, *naoSetosas, tamSetosas = 0, tamNaoSetosas = 0;
+
+        if(contaSetosas(grupos[0], tamanhos_dos_grupos[0], flores) > contaSetosas(grupos[1], tamanhos_dos_grupos[1], flores)){
+            Setosas = grupos[0];
+            tamSetosas = tamanhos_dos_grupos[0];
+            naoSetosas = grupos[1];
+            tamNaoSetosas = tamanhos_dos_grupos[1];
+        } else {
+            Setosas = grupos[1];
+            tamSetosas = tamanhos_dos_grupos[1];
+            naoSetosas = grupos[0];
+            tamNaoSetosas = tamanhos_dos_grupos[0];
+        }
+        free(tamanhos_dos_grupos); // tamanho dos grupos não vai mais ser usado   
+
+        for(int i = 0; i < tamSetosas; i++){
+            if(flores[Setosas[i]].tipo){
+                TP++;
+            } else {
+                FP++;
+            }
+        }
+
+        for(int i = 0; i < tamNaoSetosas; i++){
+            if(!flores[naoSetosas[i]].tipo){
+                TN++;
+            } else {
+                FN++;
+            }
+        }
+
+        // Guarda a acurácia nos dados a cada iteração
+        float *accuracytemp = (float*)realloc(dados->accuracies, dados->tamanho * sizeof(float));
+        if (accuracytemp == NULL){
+            printf("Falha na alocação de memória\n");
+            free(dados->accuracies);
+            exit(1);
+        }
+        dados->accuracies = accuracytemp;
+        dados->accuracies[dados->tamanho-1] = accuracy(TP,FP, TN, FN);
+
+        // printf("\nTamanho do dados = %i", dados->tamanho);a
+        // printf("\nTamanho do limiar = %f", limiar);
+        // printf("\nNumero de grupos: %i\n", num_grupos + 1);
         
         for(int i = 0; i < num_grupos; i++){ // Liberar Memoria
             free(grupos[i]);
         }
         free(grupos);
-        free(tamanhos_dos_grupos);
 
         limiar += incrementar;
         }
@@ -555,9 +652,9 @@ void gerarRelatorio(dados* dados){
     FILE *relatorio = fopen("Relatorio.csv", "w");
 
     if(relatorio){
-        fprintf(relatorio,"Limiar,Grupos\n");
+        fprintf(relatorio,"Limiar, Num de Grupos, Acurácia");
         for(int i = 0; i < dados->tamanho; i++){
-           fprintf(relatorio,"%.3f, %i\n",dados->limiares[i], dados->tamanhoGrupos[i]);
+           fprintf(relatorio,"\n%.3f, %i, %.2f",dados->limiares[i], dados->tamanhoGrupos[i], dados->accuracies[i]);
         }
     }
 
